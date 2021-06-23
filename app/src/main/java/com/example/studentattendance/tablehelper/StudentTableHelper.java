b5ef7e7f66a4ceaee3ebdda771d2569611abae8f
Pratik Patil <pratikap41@gmail.com>
2021-06-24 00:20:15 +0530

    added getAllStudent

--- app/src/main/java/com/example/studentattendance/tablehelper/StudentTableHelper.java
+++ app/src/main/java/com/example/studentattendance/tablehelper/StudentTableHelper.java
@@ -1,10 +1,14 @@
 package com.example.studentattendance.tablehelper;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.example.studentattendance.DatabaseHelper;
 import com.example.studentattendance.pojo.Student;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StudentTableHelper {
 
@@ -21,6 +25,26 @@ public class StudentTableHelper {
     public boolean addStudent( Context context, Student s){
         SQLiteDatabase db = DatabaseHelper.getInstance(context).getMyWritableDatabase();
         long suc = db.insert(tableStudent, null, s.toMap());
+        db.close();
         return  suc > 0;
     }
+
+    public List<Student> getAllStudent(Context context){
+        SQLiteDatabase db = DatabaseHelper.getInstance(context).getMyReadableDatabase();
+        List<Student> studentsList = new ArrayList<>();
+        String query = "SELECT * FROM "+tableStudent;
+        Cursor cursor = db.rawQuery(query, null);
+        if(cursor.moveToFirst()){
+            do{
+                int rollNo = cursor.getInt(0);
+                String name = cursor.getString(1);
+                String std = cursor.getString(2);
+                String division = cursor.getString(3);
+                Student s = new Student(rollNo, name, std, division);
+                studentsList.add(s);
+            }while (cursor.moveToNext());
+        }
+        return  studentsList;
+    }
+
 }
